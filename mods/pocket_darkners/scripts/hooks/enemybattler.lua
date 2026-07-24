a0fb5ee2c74074bdfdafcb42104738db1b3a10ae
fluffyboy <sekalisukarumah@gmail.com>
2026-07-24 11:57:42 +1000

    adds buster flinching(doesnt work on death)

--- mods/pocket_darkners/scripts/hooks/enemybattler.lua
+++ mods/pocket_darkners/scripts/hooks/enemybattler.lua
@@ -2,37 +2,52 @@ local EnemyBattler, super = HookSystem.hookScript(EnemyBattler)
 
 function EnemyBattler:init(...)
     super.init(self, ...)
-    self.affect_waves = false 
+    self.affect_waves = false
+    self.can_give_status = true 
+end 
+
+function EnemyBattler:hurt(amount, ...)
+    if (self.health - amount) <= 0 then 
+        Kristal.Console:warn("iudfhgkjddfhg")
+        self.can_give_status = false 
+    end 
+    super.hurt(self, amount, ...)
 end 
 
 ---@ param sYeah
-function EnemyBattler:onStatused(status) return end  
+function EnemyBattler:onStatused(status, worked) return end  
 
-function EnemyBattler:giveStatus(status, msg)
-    self:onStatused(status)
-    self:debuffEffect()
+function EnemyBattler:giveStatus(status, msg, debuff_options)
+    self:onStatused(status, self.can_give_status)
+    if self.can_give_status then 
     if status == "flinch" then 
-        self:flinch()
+    --   if love.math.random(1, 10) <= 3 then 
+        Kristal.Console:warn("raagh...")
+        self:statusMessage("msg", "flinched")
+        self:debuffEffect(debuff_options)
+        self.affect_waves = true 
+       -- end 
     end 
 end 
-
-function EnemyBattler:flinch()
-    self:statusMessage("msg", "flinched")
-    self.affect_waves = true 
 end 
 
-function EnemyBattler:debuffEffect(color, full_intensity)
+function EnemyBattler:debuffEffect(options)
+    options = options or {}
+    options.color = options.color or COLORS.red
+
     local snd = Assets.playSound("stat_fell")
     local my_fx = ShaderFX("debuff") 
-    local peak = full_intensity or 0.7
-    my_fx.shader:send("tint_color", color or COLORS.red)
+    local peak = options.full_intensity or 0.7 
+    my_fx.shader:send("tint_color", options.color)
     my_fx.shader:send("intensity", 0.0)
     my_fx.shader:send("scroll_y", 0.0)
     self:addFX(my_fx) 
+    
     local current_scroll = 0
     local duration = snd:getDuration()
     local q_duration = duration / 4
     local h_duration = duration / 2
+    
     Game.battle.timer:approach(q_duration, 0.0, peak, function(v)
         current_scroll = (current_scroll + (0.02 * DTMULT)) % 1.0
         my_fx.shader:send("scroll_y", current_scroll)
@@ -53,7 +68,6 @@ function EnemyBattler:debuffEffect(color, full_intensity)
         end)
     end)
 end 
-
 
 
 
