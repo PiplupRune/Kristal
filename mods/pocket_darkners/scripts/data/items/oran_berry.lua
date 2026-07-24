af688cd69d7a2ee6fb320172129f4843a33e3b05
fluffyboy <sekalisukarumah@gmail.com>
2026-07-24 14:09:23 +1000

    Update oran_berry.lua

--- mods/pocket_darkners/scripts/data/items/oran_berry.lua
+++ mods/pocket_darkners/scripts/data/items/oran_berry.lua
@@ -7,6 +7,7 @@ function item:init()
     self.name = "Oran Berry"
     -- Name displayed when used in battle (optional)
     self.use_name = nil
+    -- Makes it so that it is a berry.
     self.berry_type = true  
 
     -- Item type (item, key, weapon, armor)
